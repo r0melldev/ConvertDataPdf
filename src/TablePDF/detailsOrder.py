00c4fc95608c10a590d4427cb31a738d711776b3
Rommel Pumisacho <107574408+r0melldev@users.noreply.github.com>
2024-06-12 11:49:42 -0500

    [FIX]datos header

--- src/TablePDF/detailsOrder.py
+++ src/TablePDF/detailsOrder.py
@@ -14,7 +14,7 @@ def detailOrder(document, information, tuplaInformationHead, optionType):
      document.multi_cell(w = 0, h = 6, txt = 'CORTE DE TABLEROS', border = 0,
           align = 'L', fill = 6)
 
-     document.cell(w = 100, h = 6, txt = clientName , border = 0,
+     document.cell(w = 130, h = 6, txt = clientName , border = 0,
           align = 'L', fill = 6)
 
      document.cell(w = 100, h = 6, txt = proyectName, border = 0,
@@ -23,7 +23,7 @@ def detailOrder(document, information, tuplaInformationHead, optionType):
      document.multi_cell(w = 0, h = 6, txt = depName, border = 0,
           align = 'L', fill = 6)  
      
-     document.cell(w = 100, h = 6, txt = oPName, border = 0,
+     document.cell(w = 130, h = 6, txt = oPName, border = 0,
           align = 'L', fill = 6)
 
      document.multi_cell(w = 0, h = 6, txt = dateSaveName, border = 0,
